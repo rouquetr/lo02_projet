8e0f2572a2ab91c1215f67e528f6f5dc0e5ec696
IsmailMannan <33761167+IsmailMannan@users.noreply.github.com>
2017-11-20 19:52:09 +0100

    Update Rejouer.java

--- src/model/actions/Rejouer.java
+++ src/model/actions/Rejouer.java
@@ -1,10 +1,15 @@
 package model.actions;
 
+import model.joueurs.Joueur;
+
 public class Rejouer extends Action{
 
+	private static Joueur joueur; 
+	
 	public static void action() {
 		System.out.println("Vous rejouez");
-		
+		joueur.setPosition(joueur.getPosition()-1); 			//Au prochain incrément, c'est le joueur actuel qui rejoue
 	}
 	
 }
+
