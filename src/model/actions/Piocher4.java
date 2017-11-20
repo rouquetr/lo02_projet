0cce5fc542cf6da23622ae552acaef2575945cc0
IsmailMannan <33761167+IsmailMannan@users.noreply.github.com>
2017-11-20 19:51:11 +0100

    Update Piocher4.java

--- src/model/actions/Piocher4.java
+++ src/model/actions/Piocher4.java
@@ -1,10 +1,16 @@
 package model.actions;
 
+import model.cartes.Pioche;
+
 public class Piocher4 extends Action {
 
+	private static Pioche pioche;
+	
 	public static void action() {
 		System.out.println("Piocher quatre cartes");
-		
+		for (int i=0;i<=4;i++) {
+			pioche.piocher();
+		}
 	}
 	
 }
