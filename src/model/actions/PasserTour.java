a13b59c2dfe92abe4087f93fc04188336b99b761
IsmailMannan <33761167+IsmailMannan@users.noreply.github.com>
2017-11-20 19:52:53 +0100

    Update PasserTour.java

--- src/model/actions/PasserTour.java
+++ src/model/actions/PasserTour.java
@@ -1,10 +1,14 @@
 package model.actions;
 
+import model.joueurs.Joueur;
+
 public class PasserTour extends Action {
+	
+	private static Joueur joueur;
 	
 	public static void action() {
 		System.out.println("Le joueur suivant passe son tour");
-		
+		joueur.setPosition(joueur.getPosition()+2);			//Le joueur +1 passe son tour, c'est au tour du joueur +2
 	}
 
 }
