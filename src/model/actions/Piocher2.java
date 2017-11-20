fcfa1f73bbaf08a08781bd5047782b0075d9c4f6
IsmailMannan <33761167+IsmailMannan@users.noreply.github.com>
2017-11-20 19:48:48 +0100

    Update Piocher2.java

--- src/model/actions/Piocher2.java
+++ src/model/actions/Piocher2.java
@@ -1,10 +1,17 @@
 package model.actions;
 
+import model.cartes.Carte;
+import model.cartes.Pioche;
+
 public class Piocher2 extends Action {
 
+	private static Pioche pioche;
+	
 	public static void action() {
 		System.out.println("Piocher deux cartes");
-		
+		for (int i=0;i<=2;i++) {
+			pioche.piocher();
+		}
 	}
 	
 }
