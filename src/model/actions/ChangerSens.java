175b4339f077578df9551a24e25c19b7282f1481
IsmailMannan <33761167+IsmailMannan@users.noreply.github.com>
2017-11-20 19:49:36 +0100

    Update ChangerSens.java

--- src/model/actions/ChangerSens.java
+++ src/model/actions/ChangerSens.java
@@ -1,10 +1,15 @@
 package model.actions;
 
+import model.joueurs.Partie;
+
 public class ChangerSens extends Action {
 
+	private static Partie partie;
+	
 	public static void action() {
 		System.out.println("Le sens de la partie change");
-		
+		partie.changerSens();
 	}
 	
 }
+
