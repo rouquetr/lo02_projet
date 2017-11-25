6c780c0129dcadc8037dde79c7eebb947da497aa
IsmailMannan <33761167+IsmailMannan@users.noreply.github.com>
2017-11-25 16:44:23 +0100

    Update Paquet.java

--- src/model/cartes/Paquet.java
+++ src/model/cartes/Paquet.java
@@ -8,4 +8,10 @@ public abstract class Paquet extends LinkedList<Carte> {
 	public void melanger() {
 		Collections.shuffle(this);
 	}
+	public boolean ComparerCarte(Carte carteAJouer, Carte carteTalon) { 
+		if(carteTalon.equals(carteAJouer.getCouleur()) || carteTalon.equals(carteAJouer.getValeur())) {
+			return true;
+		}
+		else return false;
+	}
 }
