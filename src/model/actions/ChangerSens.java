b4448cc329214e288aa621ef3610d0d7ce31c91f
rouquetr <raphael.rouquet@utt.fr>
2017-11-28 18:02:18 +0100

    [actions] changer le sens de la partie

--- src/model/actions/ChangerSens.java
+++ src/model/actions/ChangerSens.java
@@ -3,11 +3,9 @@ package model.actions;
 import model.joueurs.Partie;
 
 public class ChangerSens implements Action {
-
-	private static Partie partie;
 	
 	public void action() {
-		partie.changerSens();
+		Partie.getInstance().changerSens();
 	}
 	
 }
